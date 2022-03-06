65ab2590a3cfc6af809ed64fa58c3b489f5ae9cc
Angryfans2 <100148250+Angryfans2@users.noreply.github.com>
2022-03-06 16:27:37 -0500

    name change

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // METAMASK CONNECTION
 const TIMEOUT = 1000;
-const COLLECTION_NAME = 'AngryFans for Ukraine';
+const COLLECTION_NAME = 'AngryFans2 for Ukraine';
 let editions = [];
 let dots = 1;
 
